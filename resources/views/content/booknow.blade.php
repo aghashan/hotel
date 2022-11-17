f80a501dcece347f6074b526d2a8c8b347a68314
aghashan <aghashan2004@gmail.com>
2022-11-17 09:31:53 +0700

    new

--- resources/views/content/booknow.blade.php
+++ resources/views/content/booknow.blade.php
@@ -28,86 +28,61 @@
                                     <input class="form-control" type="tel" placeholder="Enter your phone number">
                                 </div>
                                 <div class="form-group">
-                                    <span class="form-label">Pickup Location</span>
+                                    <span class="form-label">Nomor Kamar</span>
                                     <input class="form-control" type="text" placeholder="Enter ZIP/Location">
                                 </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6">
                                 <div class="form-group">
-                                    <span class="form-label">Destination</span>
-                                    <input class="form-control" type="text" placeholder="Enter ZIP/Location">
+                                    <span class="form-label">Check in</span>
+                                    <input class="form-control" type="date" placeholder="Enter your name">
                                 </div>
-                                <div class="row">
-                                    <div class="col-sm-5">
-                                        <div class="form-group">
-                                            <span class="form-label">Pickup Date</span>
-                                            <input class="form-control" type="date" required>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-7">
-                                        <div class="row">
-                                            <div class="col-sm-4">
-                                                <div class="form-group">
-                                                    <span class="form-label">Hour</span>
-                                                    <select class="form-control">
-                                                        <option>1</option>
-                                                        <option>2</option>
-                                                        <option>3</option>
-                                                        <option>4</option>
-                                                        <option>5</option>
-                                                        <option>6</option>
-                                                        <option>7</option>
-                                                        <option>8</option>
-                                                        <option>9</option>
-                                                        <option>10</option>
-                                                        <option>11</option>
-                                                        <option>12</option>
-                                                    </select>
-                                                    <span class="select-arrow"></span>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <div class="form-group">
-                                                    <span class="form-label">Min</span>
-                                                    <select class="form-control">
-                                                        <option>05</option>
-                                                        <option>10</option>
-                                                        <option>15</option>
-                                                        <option>20</option>
-                                                        <option>25</option>
-                                                        <option>30</option>
-                                                        <option>35</option>
-                                                        <option>40</option>
-                                                        <option>45</option>
-                                                        <option>50</option>
-                                                        <option>55</option>
-                                                    </select>
-                                                    <span class="select-arrow"></span>
-                                                </div>
-                                            </div>
-                                            <div class="col-sm-4">
-                                                <div class="form-group">
-                                                    <span class="form-label">AM/PM</span>
-                                                    <select class="form-control">
-                                                        <option>AM</option>
-                                                        <option>PM</option>
-                                                    </select>
-                                                    <span class="select-arrow"></span>
-                                                </div>
-                                            </div>
-                                        </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <span class="form-label">Check out</span>
+                                    <input class="form-control" type="date" placeholder="Enter your email">
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <div class="form-group">
+                                        <span class="form-label">Payment</span>
+                                        <select class="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                            <option>7</option>
+                                            <option>8</option>
+                                            <option>9</option>
+                                            <option>10</option>
+                                            <option>11</option>
+                                            <option>12</option>
+                                        </select>
+                                        <span class="select-arrow"></span>
                                     </div>
                                 </div>
-                                <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-3 mt-3">
-                                    <a href="" class="btn btn-primary me-md-2" type="button">Book Now</a>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <span class="form-label">Harga</span>
+                                    <input class="form-control" type="datextte" placeholder="Enter your name">
                                 </div>
-                            </form>
+                            </div>
                         </div>
                     </div>
+                    <div class="d-grid gap-2 d-md-flex justify-content-md-end mb-3 mt-3">
+                        <a href="" class="btn btn-primary me-md-2" type="button">Lanjut Pembayaran</a>
+                    </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-</div>
-</div>
-</div>
 </div>
 @endsection
